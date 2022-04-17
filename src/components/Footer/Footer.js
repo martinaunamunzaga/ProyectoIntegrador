ea569cac3e09b2968c814e10fc2753b7be43cbbf
martinaunamunzaga <69681769+martinaunamunzaga@users.noreply.github.com>
2022-04-17 14:13:33 -0300

    css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(){
     return(
-        <footer>
+        <footer className="footer">
         <ul className="team">
             <li>Nombre integrante 1</li>
             <li>Nombre integrante 2</li>
